ecb60a47093689f3599519d26d1d028add91a745
ujjawal <ujjawlg@umbrellainfocare.com>
2018-06-18 18:21:51 +0530

    Added admin user

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,14 +1,20 @@
+import { UserService } from 'src/app/user.service';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
-import { Observable } from 'rxjs';
+import { Observable ,of} from 'rxjs';
 import { ActivatedRoute } from "@angular/router";
+import { AppUser } from './models/app-user';
+import { switchMap } from "rxjs/operators"
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   user$: Observable<firebase.User>;
-  constructor(private afAuth: AngularFireAuth , private route: ActivatedRoute) {
+  constructor(
+    private userService: UserService,
+    private afAuth: AngularFireAuth , private route: ActivatedRoute) {
     this.user$ = afAuth.authState;
    }
 
@@ -21,5 +27,15 @@ export class AuthService {
 
   logout() {
     this.afAuth.auth.signOut();
+  }
+
+  get appUser$(): Observable<AppUser> {
+    return this.user$
+    .pipe(switchMap(user => {
+      if(user) return this.userService.get(user.uid).valueChanges()
+
+      return of(null);
+    })
+  );
   }
 }
