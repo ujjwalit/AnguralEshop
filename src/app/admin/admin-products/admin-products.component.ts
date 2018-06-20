1e0a0b067a87b7b2ddc41e2e46c2a37a3421922f
ujjawal <ujjawlg@umbrellainfocare.com>
2018-06-20 16:54:37 +0530

    added search functionality

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,32 @@
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: {key: string , data:{}}[];
+  subscription: Subscription;
+  filteredProducts: any[];
 
   constructor(private productService: ProductService) { 
-    this.products$ = this.productService.getAll();
+    this.subscription =  this.productService.getAll()
+    .subscribe(products => this.filteredProducts = this.products = products);
   }
 
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.data.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
